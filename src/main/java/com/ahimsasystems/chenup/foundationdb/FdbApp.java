fc96417953af49dc88f3fed8ee11e07cc8e537bf
stephenwstrom <strom@acm.org>
2025-06-14 15:37:13 -0400

    Cleaning up + add placeholder for FoundationDB.

---
+++ src/main/java/com/ahimsasystems/chenup/foundationdb/FdbApp.java
@@ -0,0 +1,27 @@
+package com.ahimsasystems.chenup.foundationdb;
+
+
+import com.apple.foundationdb.Database;
+import com.apple.foundationdb.FDB;
+import com.apple.foundationdb.tuple.Tuple;
+
+public class FdbApp {
+    public static void main(String[] args) {
+        FDB fdb = FDB.selectAPIVersion(620); // match your installed FDB version
+        try (Database db = fdb.open()) {
+            // Set a value
+            db.run(tr -> {
+                tr.set(Tuple.from("hello").pack(), Tuple.from("world").pack());
+                return null;
+            });
+
+            // Get a value
+            String value = db.read(tr -> {
+                byte[] result = tr.get(Tuple.from("hello").pack()).join();
+                return Tuple.fromBytes(result).getString(0);
+            });
+
+            System.out.println("Value for 'hello': " + value);
+        }
+    }
+}
